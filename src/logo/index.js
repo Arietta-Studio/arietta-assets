4afc0a6134a30bee56eb367bcd2905d016a3d075
Gince <ruzauskas.gintaras@gmail.com>
2024-03-19 13:56:32 +0200

    ✨ feat(assets-logo): Update license information in webfont-mono

--- src/logo/index.js
+++ src/logo/index.js
@@ -3,7 +3,7 @@ const optimizeWebp = require('./optimizeWebp');
 
 const svgList = ['logo-flat.svg', 'logo-high-contrast.svg', 'logo-text.svg'];
 
-const webpList = ['logo-3d.png'];
+const webpList = ['logo-3d.png', 'logo-squared.png'];
 
 optimizeSvg(svgList);
 optimizeWebp(webpList);
